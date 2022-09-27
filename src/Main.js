7c69ce496f89845002bb83707b46ec9ad1e4aacd
Tushar Reddy <tushar@Tushars-MacBook-Air.local>
2022-09-27 17:00:34 +0530

    added Main.js file

---
+++ src/Main.js
@@ -0,0 +1,15 @@
+    import React from "react";
+    const Main = () => {
+        return ( <div>
+            <h1>Tushar Reddy</h1>
+            <ul ClassName="header">
+                <li><a href="/">Home</a></li>
+                <li><a href="/about">About</a></li>
+                <li><a href="/contact">Contact</a></li>
+
+            </ul>
+            <div ClassName="content"></div>
+        </div> );
+    }
+     
+    export default Main;
